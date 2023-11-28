3f03a1c4c23aa18ff0b0ef1fd2110ce297b34de0
Serena Emedeke <B00141403@mytudublin.ie>
2023-11-28 19:53:35 +0000

    port

--- dbcon.php
+++ dbcon.php
@@ -3,7 +3,7 @@ $servername="localhost";
 $username="root";
 $password="";
 $dbname="football.db";
-s
+$port= "3306";
 //Create connection
 $con = mysqli_connect ($servername, $username, $password, $dbname);
 //Check connection
