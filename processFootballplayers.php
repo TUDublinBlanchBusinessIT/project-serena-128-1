38d1b1092e8298e4e33ce8e5ae4ad1b9792517c9
Serena Emedeke <B00141403@mytudublin.ie>
2023-11-28 20:09:51 +0000

    my sqli query

--- processFootballplayers.php
+++ processFootballplayers.php
@@ -1,5 +1,5 @@
 <?php
-//include ("dbcon.php");
+include ("dbcon.php");
 
 
 $firstname = $_POST['fn'];
@@ -11,5 +11,7 @@ $dateofbirth = $_POST['dob'];
 echo "$firstname $surname $dateofbirth";
 
 $sql = INSERT INTO employee(firstname,surname,dateofbirth) VALUES ('Sam','Philips','2001-03-11')
+
+mysqli_query($conn,$sql);
 
 ?>
