15ee0c2376207854f6da57861dfbcbb7064932dc
Serena Emedeke <B00141403@mytudublin.ie>
2023-11-29 20:43:02 +0000

    file

--- showFootballPlayers.php
+++ showFootballPlayers.php
@@ -5,17 +5,17 @@
 include("dbcon.php");
 $sql = "select * from employee";
 
-
+echo "<TABLE border='1'>";
 $result = mysqli_query($con,$sql);
 while($row=mysqli_fetch_assoc($result)) {
     $fn= $row['firstname'];
     $sn= $row['surname'];
     $dob= $row['dateofbirth'];
     
-    echo "$fn, $sn, $dob<br>";
+    echo "<TR><TD>$fn</TD><TD>$sn</TD><TD> $dob</TR><TD>";
 }
 
-
+echo "</TABLE>";
 
 mysqli_close($con);
 
