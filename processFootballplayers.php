65853b5f1387ecf8b8f4bf5cc9be73cb046b3170
Serena Emedeke <B00141403@mytudublin.ie>
2023-12-01 18:17:59 +0000

    mysqli_close($con); works

--- processFootballplayers.php
+++ processFootballplayers.php
@@ -1,19 +1,17 @@
 <?php
-include ("dbcon.php");
-
-
 $firstname = $_POST['fn'];
 $surname = $_POST['sn'];
 $dateofbirth = $_POST['dob'];
+
+include ("dbcon.php");
 
 echo "$firstname $surname $dateofbirth";
 
 $sql = "INSERT INTO footballplayers(firstname,surname,dateofbirth) VALUES ('Sam','Philips','2001-03-11')";
 
-if (mysqli_query($con,$sql)) {
-    echo "new record inserted";
-}
-else {
-    echo "error";
-}
+//echo $sql;
+mysqli_query($con,$sql);
+mysqli_close($con);
+
+echo "Football Player inserted";
 ?>
