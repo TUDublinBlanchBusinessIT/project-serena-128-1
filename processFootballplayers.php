c1b5f8ddfd318e3841f13dd09c47341f204d4fde
Serena Emedeke <B00141403@mytudublin.ie>
2023-12-01 18:47:50 +0000

    inserting data

--- processFootballplayers.php
+++ processFootballplayers.php
@@ -5,13 +5,11 @@ $dateofbirth = $_POST['dob'];
 
 include ("dbcon.php");
 
-echo "$firstname $surname $dateofbirth";
-
 $sql = "INSERT INTO footballplayers(firstname,surname,dateofbirth) VALUES ('$firstname','$surname','$dateofbirth')";
 
-echo $sql;
-//mysqli_query($con,$sql);
-//mysqli_close($con);
+//echo $sql;
+mysqli_query($con,$sql);
+mysqli_close($con);
 
-//echo "Football Player inserted";
+echo "Football Player inserted";
 ?>
