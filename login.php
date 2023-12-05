5e41cda508791c12f3cbe28df21bd1fef05c4b49
Serena Emedeke <B00141403@mytudublin.ie>
2023-12-05 22:32:29 +0000

    login Try Again

--- login.php
+++ login.php
@@ -1,27 +1,31 @@
 <?php
-$uswr = $_POST['username'];
+
+$usr = $_POST['username'];
 $pwd = $_POST['passwd'];
 
-require(db_con.php)
+require('db_con.php');
 
-$con= mysqli_connect(mysql_hostname,$mysql_user,$mysql_password,$mysql_database);
+$con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_database);
 
 $sql = "select pwd from registeredUser where userName='$usr'";
 
 $result = mysqli_query($con,$sql);
 
-while($row = $result- >fetch_assoc())
+while($row = $result->fetch_assoc())
 {
     $dBpwd = $row['pwd'];
-
 }
 if ($dBpwd==$pwd)
 {
-    echo "password match";
+    //echo "password match";
+    session_start();
+    $_SESSION['username'] = $usr;
+    header('Location : loggedIn.html');
 }
 else
 {
-    echo "passwords do not match";
+    //echo "passwords do not match";
+    header('Location : loginTryAgain.html');
 }
 
 
