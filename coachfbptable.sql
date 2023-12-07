70ff78c87c6e8170f332bbb1c694c2bb5365798d
Serena Emedeke <B00141403@mytudublin.ie>
2023-12-07 23:49:47 +0000

    2 table works on sql heidi

---
+++ coachfbptable.sql
@@ -0,0 +1,25 @@
+DROP DATABASE footballplayers;
+CREATE DATABASE footballplayers;
+USE footballplayers;
+
+CREATE TABLE coach (
+id INT AUTO_INCREMENT,
+firstname VARCHAR(30),
+surname VARCHAR(30),
+mobilephone VARCHAR(30),
+PRIMARY KEY(id) 
+);
+
+CREATE TABLE footballplayer (
+id INT AUTO_INCREMENT,
+firstname VARCHAR(30),
+surname VARCHAR(30),
+dateofbirth DATE, 
+height DECIMAL(3,2),
+coachid INT,
+FOREIGN KEY(coachid) REFERENCES coach(id),
+PRIMARY KEY(id)
+);
+
+INSERT INTO coach(firstname,surname,mobilephone) VALUES ("Gemma","Owens", "0891114568");
+INSERT INTO footballplayer(firstname,surname,dateofbirth,height,coachid) VALUES ('Jennifer','Lopez','2002-01-01', 1.63, 1);
