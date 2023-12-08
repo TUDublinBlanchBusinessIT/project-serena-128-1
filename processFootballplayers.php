8f377e28237edcf099169f48f7d80b21a94c696c
Serena Emedeke <B00141403@mytudublin.ie>
2023-12-08 11:43:51 +0000

    added another row to footballplayer table works

--- processFootballplayers.php
+++ processFootballplayers.php
@@ -3,14 +3,16 @@ $firstname = $_POST['fn'];
 $surname = $_POST['sn'];
 $dateofbirth = $_POST['dob'];
 $height = $_POST['hght'];
+$gender = $_POST['gender'];
 
 include ("dbcon.php");
 
-$sql =INSERT INTO footballplayer(firstname,surname,dateofbirth,height) VALUES ('$firstname','$surname','$dateofbirth','$height')";
+$sql ="INSERT INTO footballplayer (firstname,surname,dateofbirth,height,gender) VALUES ('$firstname','$surname','$dateofbirth','$height','$gender')";
 
 echo $sql;
 mysqli_query($con,$sql);
 mysqli_close($con);
+
 
 echo "Football Player inserted";
 ?>
